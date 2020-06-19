65f7f4873e53034be718389a5772f939d61ba488
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-06-18 20:15:41 -0500

    Added base html and styles

---
+++ scripts/functions.js
@@ -0,0 +1,15 @@
+/**** Starts Marvin ****/
+
+/**** Ends Marvin ****/
+
+/**** Starts Victor ****/
+
+/**** Ends Victor ****/
+
+/**** Starts Zamir ****/
+
+/**** Ends Zamir ****/
+
+/**** Starts General ****/
+window.onload = () => {};
+/**** Ends General ****/
